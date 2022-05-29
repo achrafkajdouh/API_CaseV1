04806fd393266d06d44940ee028ec2018fbef106
achraf k <achrafkajdouh@gmail.com>
2022-05-29 17:50:19 +0200

    Added model

---
+++ API_CaseV1/Models/Adressen.cs
@@ -0,0 +1,12 @@
+﻿namespace API_CaseV1.Models
+{
+    public class Adressen
+    {
+        public long Id { get; set; }
+        public string? Straat { get; set; }
+        public int Huisnummer { get; set; }
+        public string? Postcode { get; set; }
+        public string? Plaats { get; set; }
+        public string? Land { get; set; }
+    }
+}
