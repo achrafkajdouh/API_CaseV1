1d855d11257e0af2389f0cadd66474311a4cd6be
achraf k <achrafkajdouh@gmail.com>
2022-05-31 10:23:24 +0200

    Added Controller for Geolocation Added Geolocation db

---
+++ API_CaseV1/Controllers/GeoController.cs
@@ -0,0 +1,24 @@
+﻿/*
+using MaxMind.GeoIP2;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_CaseV1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeoController : ControllerBase
+    {
+        [HttpGet("[action']/{ipAddress}")]
+        public IActionResult GetCountry(string ipAddress)
+        {
+            using (var reader = new DatabaseReader(@"C:\Users\Achraf\source\repos\API_CaseV1\DB\geo.mmd"))
+            {
+                var response = reader.Country("ipAddress");
+
+                return StatusCode(StatusCodes.Status200OK, response.Country.Name);
+            }
+        }
+    }
+}
+*/
